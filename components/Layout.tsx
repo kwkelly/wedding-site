694827f71dd2e30945f1a4b40f0594ce9af89510
Keith Kelly <keith@helmholtz.local>
2021-05-02 08:28:50 -0400

    fix: move fonts to _document head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,49 +11,7 @@ const Layout = ({ children, title }: Props) => (
   <>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta
-        name="google-site-verification"
-        content="sgp30ifTdqrNqKJKuVQgiFBrRZ7XhhrwAJV4kDkaHu8"
-      />
-      <meta
-        name="Description"
-        content="Information about the wedding of Elizabeth Cassidy and Keith Kelly."
-      />
-      <link
-        rel="shortcut icon"
-        sizes="192x192"
-        type="image/png"
-        href="/favicon-192x192.png"
-      />
-      <link
-        rel="shortcut icon"
-        sizes="512x512"
-        type="image/png"
-        href="/favicon-512x512.png"
-      />
-      <link
-        rel="preload"
-        href="/fonts/lightheartedly/Lightheartedly.woff"
-        as="font"
-        type="font/woff"
-        crossOrigin="anonymous"
-      ></link>
-      <link
-        rel="preload"
-        href="/fonts/whitney/whitneybook.woff"
-        as="font"
-        type="font/woff"
-        crossOrigin="anonymous"
-      ></link>
-      <link
-        rel="preload"
-        href="/fonts/whitney/whitneylight.woff"
-        as="font"
-        type="font/woff"
-        crossOrigin="anonymous"
-      ></link>
     </Head>
     <header></header>
     {children}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,51 @@ export default class MyDocument extends Document<{ styleTags: any }> {
   render() {
     return (
       <Html lang="en">
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <meta charSet="utf-8" />
+          <meta
+            name="google-site-verification"
+            content="sgp30ifTdqrNqKJKuVQgiFBrRZ7XhhrwAJV4kDkaHu8"
+          />
+          <meta
+            name="Description"
+            content="Information about the wedding of Elizabeth Cassidy and Keith Kelly."
+          />
+          <link
+            rel="shortcut icon"
+            sizes="192x192"
+            type="image/png"
+            href="/favicon-192x192.png"
+          />
+          <link
+            rel="shortcut icon"
+            sizes="512x512"
+            type="image/png"
+            href="/favicon-512x512.png"
+          />
+          <link
+            rel="preload"
+            href="/fonts/lightheartedly/Lightheartedly.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/whitney/whitneybook.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/whitney/whitneylight.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
